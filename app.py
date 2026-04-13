4b03a031764f943d0f95584bcd5a4d850eac7fbf
Arch-cmd2 <karitodark0@gmail.com>
2026-04-13 21:58:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 @app.route("/")
 def index():
-    return send_from_directory("templates", "index.html")
+    return send_from_directory(os.path.join(BASE_DIR, "templates"), "index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
